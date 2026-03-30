023f05d1eb1ee7520ba35e2b3fac7a28121a811f
SanchitKumbhar <sanchitskumbhar@gmail.com>
2026-03-30 12:55:01 +0530

    dep

--- index.js
+++ index.js
@@ -23,16 +23,20 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+
 const io = new Server(server, {
 	cors: {
-		origin: "*",
-		methods: ["GET", "POST"]
+		origin: "http://13.201.114.37",
+		methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"]
 	}
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+	origin: "http://13.201.114.37",
+	methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
